cc7b9b046e96cf93b7e93660c2fae9023ac0c288
menkin <menkin.steven@outlook.com>
2024-01-01 12:37:49 -0600

    Added Routing and Articles Nav Link

--- FullStack.UI/src/app/app.routes.ts
+++ FullStack.UI/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './components/Employees/employee-list/employee-list.component';
+import { ExecutiveListComponent } from './components/Executives/executive-list/executive-list.component';
+import { ArticlesComponent } from './components/Articles/articles.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'employee-list', component: EmployeeListComponent},
+    {path: 'executive-list', component: ExecutiveListComponent},
+    {path: 'articles', component: ArticlesComponent}
+];
