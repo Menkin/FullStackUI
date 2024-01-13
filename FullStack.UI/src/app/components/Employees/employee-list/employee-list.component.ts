3e5663e87f27cfa1dd5b5c26f9c9bd6527c459e6
menkin <menkin.steven@outlook.com>
2024-01-13 13:28:07 -0600

    implemented a employee object within the typescript employee component

--- FullStack.UI/src/app/components/Employees/employee-list/employee-list.component.ts
+++ FullStack.UI/src/app/components/Employees/employee-list/employee-list.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../../../models/employee.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
-export class EmployeeListComponent {
 
+export class EmployeesListComponent {
+
+  employees: Employee[] = [
+    {
+      id: '85f94861-98b7-44f5-bb49-3318e20537a2',
+      name: 'Scott',
+      email: 'Scott.Garage@water.com',
+      phone: 4698620082,
+      salary: 60000,
+      department: 'IT'
+    },
+    {
+      id: '0f374207-3cf9-4ace-9039-36a08931e035',
+      name: 'Sarah',
+      email: 'Sarah.Bizsichi@aol.com',
+      phone: 7825866941,
+      salary: 35000,
+      department: 'Human Resources'
+    },
+    {
+      id: 'd4231998-00e6-46a1-96ef-ada0e7d993e6',
+      name: 'Brock',
+      email: 'Brock.Zalinger@gmail.com',
+      phone: 8729991245,
+      salary: 80000,
+      department: 'IT'
+    }
+  ];
+  constructor() { }
+  
+  ngOnInit(): void {
+
+    
+    
+  }
 }
+
+
