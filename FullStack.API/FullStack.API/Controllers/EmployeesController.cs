bab02269ff5ecdb1be668021a6bc173f684f50a3
menkin <menkin.steven@outlook.com>
2024-01-16 09:54:38 -0600

    Added Employees API Controller with CRUD operations. Utilized [ApiController] attribute for controller identification.

---
+++ FullStack.API/FullStack.API/Controllers/EmployeesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FullStack.API.Controllers
+{
+    [ApiController]
+    public class EmployeesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            
+        }
+    }
+}
